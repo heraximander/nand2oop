1060cb8e3a902dd1947535ef8c59c52a96029aea
heraximander <170171217+heraximander@users.noreply.github.com>
2024-08-18 12:08:20 +0800

    Add pre-commit script

--- hdl-macro/src/lib.rs
+++ hdl-macro/src/lib.rs
@@ -3,7 +3,8 @@ use proc_macro2::Span;
 use quote::quote;
 use syn::{Ident, ItemFn, LitInt, LitStr};
 
-const CHIP_FN_TYPE_ERR: &str = "chip function must return type [ChipOutputInner;n] where n is a literal greater than 0";
+const CHIP_FN_TYPE_ERR: &str =
+    "chip function must return type [ChipOutputInner;n] where n is a literal greater than 0";
 const CHIP_ARG_TYPE_ERR: &str = "chip function must take arguments of &Bump,[Input<'_>;n] where _n_ is a literal greater than 0";
 
 #[proc_macro_attribute]
@@ -11,7 +12,14 @@ pub fn chip(_: TokenStream, item: TokenStream) -> TokenStream {
     let ast: ItemFn = syn::parse(item).unwrap();
     let ident = &ast.sig.ident;
     let name = ident.to_string();
-    let struct_name_str = &(name.chars().take(1).next().unwrap().to_uppercase().to_string() + &name[1..]);
+    let struct_name_str = &(name
+        .chars()
+        .take(1)
+        .next()
+        .unwrap()
+        .to_uppercase()
+        .to_string()
+        + &name[1..]);
     let struct_name = Ident::new(struct_name_str, ast.sig.ident.span());
 
     assert_eq!(ast.sig.inputs.len(), 2, "{}", CHIP_ARG_TYPE_ERR);
@@ -21,30 +29,30 @@ pub fn chip(_: TokenStream, item: TokenStream) -> TokenStream {
             syn::Type::Array(tya) => match tya.len {
                 syn::Expr::Lit(x) => match x.lit {
                     syn::Lit::Int(i) => i,
-                    _ => panic!("{}", CHIP_ARG_TYPE_ERR)
+                    _ => panic!("{}", CHIP_ARG_TYPE_ERR),
                 },
-                _ => panic!("{}", CHIP_ARG_TYPE_ERR)
+                _ => panic!("{}", CHIP_ARG_TYPE_ERR),
             },
             _ => panic!("{}", CHIP_ARG_TYPE_ERR),
         },
     };
-    
+
     let return_size: usize = match ast.sig.output {
         syn::ReturnType::Default => panic!("{}", CHIP_FN_TYPE_ERR),
         syn::ReturnType::Type(_, ref ty) => match *(ty.clone()) {
             syn::Type::Array(tya) => match tya.len {
                 syn::Expr::Lit(x) => match x.lit {
                     syn::Lit::Int(i) => str::parse(&i.to_string()).expect(CHIP_FN_TYPE_ERR),
-                    _ => panic!("{}", CHIP_FN_TYPE_ERR)
+                    _ => panic!("{}", CHIP_FN_TYPE_ERR),
                 },
-                _ => panic!("{}", CHIP_FN_TYPE_ERR)
+                _ => panic!("{}", CHIP_FN_TYPE_ERR),
             },
             _ => panic!("{}", CHIP_FN_TYPE_ERR),
         },
     };
     let return_size_literal = LitInt::new(&return_size.to_string(), Span::call_site());
     let lit_name = LitStr::new(struct_name_str, Span::call_site());
-    let lit_id = LitStr::new(&format!("{}{{}}",struct_name_str), Span::call_site());
+    let lit_id = LitStr::new(&format!("{}{{}}", struct_name_str), Span::call_site());
 
     let gen = quote! {
         struct #struct_name<'a> {
@@ -79,7 +87,7 @@ pub fn chip(_: TokenStream, item: TokenStream) -> TokenStream {
             fn get_id(&self) -> String {
                 format!(#lit_id, self.identifier)
             }
-            
+
             fn get_label(&self) -> &'static str {
                 #lit_name
             }
